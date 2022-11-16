bf8036535845f024fde9d9e9130eea77ff216bad
kimjihyun <91wlgus@naver.com>
2022-11-16 16:46:50 +0900

    adding jsonfile

--- final_pjt_back/TMDBapi.py
+++ final_pjt_back/TMDBapi.py
@@ -3,8 +3,10 @@ import json
 import copy
 from pprint import pprint
 
-file_path_popular_movie = 'popular_movie.json'
-file_path_genre = 'genre.json'
+API_KEY = '90fd11e81789473ce37dc17f57e12a12'
+
+popular_movies = 'popular_movie.json'
+movies_genre = 'genre.json'
 
 def TMDBapi():
     result = []
@@ -12,19 +14,21 @@ def TMDBapi():
     pages = 10
 
     for page in range(1,pages+1):
-        URL = 'https://api.themoviedb.org/3/movie/popular?api_key=a41ba99ef968eda4eec19375d33b98ec&language=ko-KR&page='+str(page)
+        URL = f'https://api.themoviedb.org/3/movie/popular?api_key={API_KEY}&language=ko-KR&page='+str(page)
         response = requests.get(URL).json()['results']
+#         pprint(response)
         for i in range(len(response)):
             del response[i]["backdrop_path"]
             del response[i]["original_title"]
             del response[i]["original_language"]
             del response[i]["video"]
-            del response[i]["vote_count"]
-            response[i]["tmdb_id"] = response[i]["id"]
+            # del response[i]["vote_count"]
+            response[i]["movie_id"] = response[i]["id"]
             del response[i]["id"]
+            del response[i]["genre_ids"]
             response[i]["poster_path"] = 'https://image.tmdb.org/t/p/w500'+response[i]["poster_path"]
             templates = {
-                "model": "movieapi.movie",
+                "model": "movies.movies_movie",
                 "pk": 0,
                 "fields": {
                 }
@@ -33,24 +37,49 @@ def TMDBapi():
             templates["pk"] = idx
             templates["fields"] = response[i]
             result.append(templates)
+        # pprint(result)
     return result
+# TMDBapi()
 
-def TMDBapiGenre():
-    URL = 'https://api.themoviedb.org/3/genre/movie/list?api_key=a41ba99ef968eda4eec19375d33b98ec&language=ko-KR'
-    response = requests.get(URL).json()['genres']
+# def TMDBapiGenre():
+#     URL = 'https://api.themoviedb.org/3/genre/movie/list?api_key={API_KEY}&language=ko-KR'
+#     response = requests.get(URL).json()['genres']
 
-    for genre in response:
-        temp= copy.deepcopy(genre)
-        del genre['id']
-        del genre['name']
-        genre['model'] = 'movieapi.genre'
-        genre['fields'] = temp
+#     for genre in response:
+#         temp= copy.deepcopy(genre)
+#         del genre['id']
+#         del genre['name']
+#         genre['model'] = 'movies.movies_genre'
+#         genre['fields'] = temp
 
-    return response
+#     return response
 
 
-with open(file_path_popular_movie, 'w') as f:
-    json.dump(TMDBapi(),f,ensure_ascii=False,indent=4)
+with open(popular_movies, 'w',encoding='utf=8') as file:
+    json.dump(TMDBapi(),file,ensure_ascii=False,indent=4)
  
-with open(file_path_genre, 'w') as f:
-    json.dump(TMDBapiGenre(),f,ensure_ascii=False,indent=4)
+# with open(movies_genre, 'w',encoding='utf=8') as file:
+#     json.dump(TMDBapiGenre(),file,ensure_ascii=False,indent=4)
+'''
+'''
+
+'''
+ {'adult': False,
+  '###backdrop_path': '/xJHokMbljvjADYdit5fK5VQsXEG.jpg',
+  'genre_ids': [12, 18, 878],
+  'id': 157336,
+  '###original_language': 'en',
+  '###original_title': 'Interstellar',
+  'overview': '세계 각국의 정부와 경제가 완전히 붕괴된 미래가 다가온다. 지난 20세기에 범한 잘못이 전 세계적인 식량 부족을 '
+              '불러왔고, NASA도 해체되었다. 나사 소속 우주비행사였던 쿠퍼는 지구에 몰아친 식량난으로 옥수수나 키우며 살고 '
+              '있다. 거센 황사가 몰아친 어느 날 알 수 없는 힘에 이끌려 딸과 함께 도착한 곳은 인류가 이주할 행성을 찾는 '
+              '나사의 비밀본부. 이 때 시공간에 불가사의한 틈이 열리고, 이 곳을 탐험해 인류를 구해야 하는 임무를 위해 쿠퍼는 '
+              '만류하는 딸을 뒤로한 채 우주선에 탑승하는데...',
+  'popularity': 212.5,
+  'poster_path': '/zDNAeWU0PxKolEX1D8Vn1qWhGjH.jpg',
+  'release_date': '2014-11-05',
+  'title': '인터스텔라',
+  '###video': False,
+  'vote_average': 8.4,
+  'vote_count': 29785},
+'''
--- final_pjt_back/movies/models.py
+++ final_pjt_back/movies/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class Genre(models.Model):
+    pass
+
+class Movie(models.Model):
+    # user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    movie_id = models.IntegerField() # 영화 id
+    title = models.CharField(max_length=100) # 제목
+    adult = models.BooleanField()
+    # genre_ids = models.ManyToManyField(Genre)
+    poster_path = models.TextField()
+    overview = models.TextField() # 줄거리
+    popularity = models.FloatField()
+    release_date = models.DateField(auto_now=False, auto_now_add=False)
+    vote_average = models.FloatField()
+    vote_count = models.IntegerField()
+    
+
+'''
+'adult': False,
+'movie_id': 20982,
+'title': '나루토 질풍전 극장판 1기 - 나루토의 죽음',
+'genre_ids': [16, 28, 14],
+'poster_path': 'https://image.tmdb.org/t/p/w500/vaEX9f8ud7iF0G7EoyGxAyRNV1p.jpg',
+'overview': '미래 예언과 마귀 봉인 능력을 지닌 원령나라의 무녀가 부활한 마귀의 표적으로 지목되자 나루토와 '
+            '친구들은 무녀를 지키기 위해 길을 떠난다. 그러나 원령나라에 도착한 일행에게 무녀는 나루토의 '
+            '죽음을 예언하고 이에 모두들 충격에 휩싸이는데...',
+'popularity': 217.402,
+'release_date': '2007-08-04',
+'vote_average': 7.3,
+'vote_count': 519},
+'''
