38296e399c7b1f2cf8a59c02f2dde0bfab8eb5d7
luminaries <luminaries1@naver.com>
2022-11-16 14:04:20 +0900

    django_start

---
+++ final_pjt_back/movies/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path,include
+from . import views
+
+urlpatterns = [
+    path('movies/', views.movies),
+    # path('movies/<int:movies_pk>/', views.movies_detail),
+    # path('comments/', views.comment_list),
+    # path('comments/<int:comment_pk>/', views.comment_detail),
+    # path('movies/<int:movies_pk>/comments/', views.comment_create),
+    # # 필수 작성
+    # path('schema/', SpectacularAPIView.as_view(), name='schema'),
+    # # optional UI
+    # path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+]
