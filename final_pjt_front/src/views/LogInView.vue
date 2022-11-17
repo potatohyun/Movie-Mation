6e7039f61839a7813090445a6f7d5177f6401cba
leejy <leejy96kr@naver.com>
2022-11-17 12:13:42 +0900

    vue loginsignup 100 ljy

--- final_pjt_front/src/views/LogInView.vue
+++ final_pjt_front/src/views/LogInView.vue
@@ -1,15 +1,37 @@
 <template>
   <div>
-    <h1>Login</h1>
+    <form @submit.prevent="logIn">
+      <label for="username">username : </label>
+      <input type="text" id="username" v-model="username">
+      <hr>
+      <label for="password"> password : </label>
+      <input type="password" id="password" v-model="password">
+      <hr>
+      <input type="submit" value="logIn">
+    </form>
   </div>
 </template>
 
 <script>
 export default {
-    name: 'LogInView',
+  name: 'LogInView',
+  data() {
+    return {
+      username: null,
+      password: null,
+    }
+  },
+  methods: {
+    logIn() {
+      const username = this.username
+      const password = this.password
+
+      const payload = {
+        username: username,
+        password: password,
+      }
+      this.$store.dispatch('logIn', payload)
+    }
+  }
 }
 </script>
-
-<style>
-
-</style>
--- final_pjt_front/src/views/SignUpView.vue
+++ final_pjt_front/src/views/SignUpView.vue
@@ -1,16 +1,49 @@
 <template>
   <div>
     <h1>계정 생성</h1>
+    <form @submit.prevent="signUp">
+      <label for="username">username : </label>
+      <input type="text" id="username" v-model="username">
+      <hr>
+      <label for="password1"> password : </label>
+      <input type="password" id="password1" v-model="password1">
+      <hr>
+      <label for="password2"> password confirmation : </label>
+      <input type="password" id="password2" v-model="password2">
+      
+      <input type="submit" value="SignUp">
+    </form>
   </div>
 </template>
 
 <script>
 export default {
-    name: 'SignUpView'
+  name: 'SignUpView',
+  data() {
+    return {
+      username: null,
+      password1: null,
+      password2: null,
+    }
+  },
+  methods: {
+    signUp() {
+      const username = this.username
+      const password1 = this.password1
+      const password2 = this.password2
 
+      const payload = {
+        // username,
+        // password1,
+        // password2,
+        username: username,
+        password1: password1,
+        password2: password2,
+      }
+
+      this.$store.dispatch('signUp', payload)
+
+    }
+  }
 }
 </script>
-
-<style>
-
-</style>
