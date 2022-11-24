6adb5ce9d224dfa604c4b24dc4052604109b70a5
leejy96kr <leejy96kr@naver.com>
2022-11-25 01:48:48 +0900

    댓글리스트출력 도전 ljy

---
+++ final_pjt_front/src/components/MyReviewListItem.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <b>{{myreview.title}}</b>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'MyReviewListItem',
+    props:{
+        myreview:Object
+    },
+    // data(){
+
+    // },
+    // methods:{
+
+    // }
+
+}
+</script>
+
+<style>
+
+</style>
